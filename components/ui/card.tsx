11734dbfdb94dfc990f42926eea8f1bdaf9bc781
daduyun <mqxuty@gmail.com>
2025-04-21 11:58:50 +0800

    Add files via upload

---
+++ components/ui/card.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Card({ className = "", children }) {
+  return <div className={`rounded-2xl border p-4 bg-white ${className}`}>{children}</div>;
+}
+
+export function CardContent({ className = "", children }) {
+  return <div className={className}>{children}</div>;
+}
