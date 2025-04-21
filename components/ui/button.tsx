c15606ff42b6dd120cb69545d6009de945d37add
daduyun <mqxuty@gmail.com>
2025-04-21 12:26:25 +0800

    Delete components/ui/button.tsx

--- components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Button({ children, onClick, className = "" }) {
-  return (
-    <button onClick={onClick} className={`bg-blue-600 text-white px-4 py-2 rounded-xl ${className}`}>
-      {children}
-    </button>
-  );
-}
